93a31f55c985a478fdf2b66d3569d87e6276e6ad
Tatsuya Toyama <toyama.tatsuya@itolab.nitech.ac.jp>
2017-02-15 15:29:43 +0900

    フォトライブラリ更新

--- js/mymove.js
+++ js/mymove.js
@@ -27,6 +27,85 @@ function pop(self) {
     $('#imagemodal').modal('show');
 }
 
+// フォトライブラリー内の追従するメニュー用
+jQuery(document).ready(function($){
+	//variables
+	var $window = $(window);
+	var $menu = $("#photoes_menu");
+	var $photoes = $("#photoes");
+	var window_min = 0;
+	var window_max = 0;
+	var threshold_offset = 10;
+	/*
+	 set the container's maximum and minimum limits as well as movement thresholds
+	*/
+	function set_limits(){
+		//max and min container movements
+		var max_move = $photoes.offset().top + $photoes.height() - $menu.height() - 2*parseInt($menu.css("top") );
+		var min_move = $photoes.offset().top;
+		//save them
+		$menu.attr("data-min", min_move).attr("data-max",max_move);
+		//window thresholds so the movement isn't called when its not needed!
+		//you may wish to adjust the freshhold offset
+		window_min = min_move - threshold_offset;
+		window_max = max_move + $menu.height() + threshold_offset;
+	}
+	//sets the limits for the first load
+	set_limits();
+
+	function window_scroll(){
+		//if the window is within the threshold, begin movements
+		if( $window.scrollTop() >= window_min && $window.scrollTop() < window_max ){
+			//reset the limits (optional)
+			set_limits();
+			//move the container
+			container_move();
+		}
+	}
+	$window.bind("scroll", window_scroll);
+
+	/**
+	 * Handles moving the container if needed.
+	**/
+	function container_move(){
+		var wst = $window.scrollTop();
+		//if the window scroll is within the min and max (the container will be "sticky";
+
+    if (0){
+      if( wst >= $menu.attr("data-min") && wst <= $menu.attr("data-max") ){
+        $menu.addClass('fixed');
+      } else if( wst <= $menu.attr("data-min") ){
+        $menu.removeClass('fixed');
+
+      } else if( wst > $menu.attr("data-max") ){
+        $menu.removeClass('fixed');
+        $menu.css("margin-top", $menu.attr("data-max")-$menu.attr("data-min")+"px" );
+      }
+    }
+
+    if (1){
+  		if( wst >= $menu.attr("data-min") && wst <= $menu.attr("data-max") ){
+  			//work out the margin offset
+  			var margin_top = $window.scrollTop() - $menu.attr("data-min");
+  			//margin it down!
+  			$menu.css("margin-top", margin_top);
+
+  		//if the window scroll is below the minimum
+  		}else if( wst <= $menu.attr("data-min") ){
+  			//fix the container to the top.
+  			$menu.css("margin-top",0);
+
+  		//if the window scroll is above the maximum
+  		}else if( wst > $menu.attr("data-max") ){
+  			//fix the container to the top
+  			$menu.css("margin-top", $menu.attr("data-max")-$menu.attr("data-min")+"px" );
+  		}
+    }
+	}
+	//do one container move on load
+	container_move();
+});
+
 // プロジェクトカード
 var json_url = 'http://private-c40429-missionforest.apiary-mock.com/missions';
 $(window).load(function (){
